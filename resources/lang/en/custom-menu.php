941f4e9a8d03ed79c8c1ed40c14e05c0e7d4aa02
Grisha Belinsky <grishabelinsky@hotmail.com>
2020-11-25 20:24:14 +0200

    update

--- resources/lang/en/custom-menu.php
+++ resources/lang/en/custom-menu.php
@@ -11,14 +11,3 @@ return array (
     'about-us' => 'About Us',
   ),
 );
-return array (
-  'nav-menu' => 
-  array (
-    'blog' => 'Blog',
-    'courses' => 'Courses',
-    'bundles' => 'Bundles',
-    'forums' => 'Forums',
-    'contact' => 'Contact',
-    'about-us' => 'About Us',
-  ),
-);
