06124a2168bc93d02a010c3e19a2d3f9cfceb697
Grisha Belinsky <grishabelinsky@hotmail.com>
2021-01-04 08:30:29 +0200

    update

--- resources/views/backend/users/edit.blade.php
+++ resources/views/backend/users/edit.blade.php
@@ -68,13 +68,13 @@
             @endcan
 
             @can('user_delete')
-            <div class="row" role="tablist">
+            <!-- <div class="row" role="tablist">
                 <div class="col-auto">
                     {!! Form::open(['method' => 'DELETE','route' => ['admin.users.destroy', $user->id],'style'=>'display:inline']) !!}
                     {!! Form::submit('Delete', ['class' => 'btn btn-outline-secondary']) !!}
                     {!! Form::close() !!}
                 </div>
-            </div>
+            </div> -->
             @endcan
         </div>
     </div>
