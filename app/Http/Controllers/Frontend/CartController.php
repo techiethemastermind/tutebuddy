9aa93d9712bfe1188bc7b0f99882903fdbf2a048
Grisha Belinsky <grishabelinsky@hotmail.com>
2021-01-11 19:01:09 +0200

    update

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Cart;
+use Cookie;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
